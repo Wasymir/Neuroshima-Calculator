c8ba13ca49c4e293e5cb9a6d678dee36c7dde2ed
Wasymir <72921498+Wasymir@users.noreply.github.com>
2021-11-28 21:24:18 +0100

    I guess its working

---
+++ README.md
@@ -0,0 +1,27 @@
+# **ENG:** A Simple DC calclculator for Neuroshima - Polish Rpg System.
+---
+
+# **PL:** Prosty Kalkulator Poziomów Trudności Dla Neuroshimy
+
+---
+
+## Użycie:
+
+Po prostu uruchom program przy użyciu terminala z wartością parametru jako argumentem.
+
+Input:
+
+> ns.exe 15
+
+Output:
+
+>łatwy -> 17
+przeciętny -> 5
+problematyczny -> 13
+trudny -> 10
+bardzo trudny -> 7
+cholernie trudny -> 4
+fart -> 0
+mistrzowski -> -5
+arcymistrzowski -> -9
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    if let Some(arg) = env::args().collect::<Vec<String>>().get(1) {
+        if let Ok(value) = arg.parse::<i32>() {
+            for (l, m) in [
+                ("łatwy", 2),
+                ("przeciętny", 0),
+                ("problematyczny", -2),
+                ("trudny", -5),
+                ("bardzo trudny", -8),
+                ("cholernie trudny", -11),
+                ("fart", -15),
+                ("mistrzowski", -20),
+                ("arcymistrzowski", -24),
+            ]
+            .iter()
+            {
+                println!("{} -> {:?}", l, value + m);
+            }
+        } else {
+            println!("Wprowadź poprawny numer");
+        }
+    } else {
+        println!("Wprowadź wartość parametru bohatera");
+    }
 }
